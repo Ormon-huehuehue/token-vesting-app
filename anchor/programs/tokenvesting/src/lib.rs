7e2eb3a741558f9d984e9202b6fc688d1cdbdc59
Ormon-huehuehue <armnvrma10@gmail.com>
2025-02-05 14:49:40 +0530

    logic to claim tokens

--- anchor/programs/tokenvesting/src/lib.rs
+++ anchor/programs/tokenvesting/src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(clippy::result_large_err)]
 
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
+use anchor_spl::{associated_token::AssociatedToken, token_interface::{Mint, TokenAccount, TokenInterface}};
 
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
@@ -14,11 +14,34 @@ pub mod tokenvesting {
       *ctx.accounts.vesting_account = VestingAccount{
         owner : ctx.accounts.signer.key(),
         mint: ctx.accounts.mint.key() ,
-        treasury_token_amount : ctx.accounts.treasury_token_account.key(),
+        treasury_token_account : ctx.accounts.treasury_token_account.key(),
         company_name,
-        treasury_bump : ctx.bumps.treasury_token_account,
+        treasury_bump : ctx.bumps.treasury_token_account, 
         bump : ctx.bumps.vesting_account
       };
+
+      Ok(())
+    }
+
+    pub fn create_employee_account( ctx : Context<CreateEmployeeAccount>, start_time : i64, end_time : i64, cliff_time : i64, total_amount : u64 ) -> Result<()>{
+
+      *ctx.accounts.employee_account = EmployeeAccount{
+        beneficiary : ctx.accounts.beneficiary.key(),
+        start_time,
+        end_time,
+        cliff_time,
+        vesting_account : ctx.accounts.vesting_account.key(),
+        total_amount,
+        total_withdrawn : 0,
+        bump : ctx.bumps.employee_account
+      };
+
+      Ok(())
+    }
+
+
+    pub fn claim_tokens( ctx : Context<ClaimTokens>, company_name : String) -> Result<()> {
+
 
       Ok(())
     }
@@ -55,9 +78,6 @@ pub struct CreateVestingAccount<'info>{
 
   pub system_program : Program<'info, System>,
   pub token_program : Interface<'info, TokenInterface>
-
-
-
 }
 
 
@@ -66,13 +86,93 @@ pub struct CreateVestingAccount<'info>{
 pub struct VestingAccount{
   pub owner : Pubkey,
   pub mint : Pubkey,
-  pub treasury_token_amount : Pubkey,
+  pub treasury_token_account : Pubkey,
   #[max_len(50)]
   pub company_name : String,
   pub treasury_bump : u8,
   pub bump : u8
-
 }
 
 
+#[derive(Accounts)]
+#[instruction(start_time : i64, end_time : i64, cliff_time : i64, total_amount : u64)]
+pub struct CreateEmployeeAccount<'info>{
+  #[account(mut)]
+  pub owner : Signer<'info>,
+  pub beneficiary : SystemAccount<'info>,
+
+  #[account(
+    has_one = owner
+  )]
+  pub vesting_account : Account<'info, VestingAccount>,
+
+  #[account(
+    init, 
+    space = 8 + EmployeeAccount::INIT_SPACE,
+    payer = owner,
+    seeds = [b"employee_vesting",beneficiary.key().as_ref(), vesting_account.key().as_ref()],
+    bump
+  )]
+  pub employee_account : Account<'info, EmployeeAccount>,
+
+  pub system_program : Program<'info, System> 
+
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct EmployeeAccount{
+  pub beneficiary : Pubkey,
+  pub start_time : i64,
+  pub end_time : i64,
+  pub cliff_time : i64,
+  pub vesting_account : Pubkey,
+  pub total_amount : u64,
+  pub total_withdrawn : u64,
+  bump : u8
+}
+
+
+#[derive(Accounts)]
+#[instruction(company_name : String)]
+pub struct ClaimTokens<'info>{
+  #[account(mut)]
+  pub beneficiary : Signer<'info>,
+
+  #[account(
+    mut,
+    seeds = [b"employee_vesting",beneficiary.key().as_ref(), vesting_account.key().as_ref()],
+    bump = employee_account.bump,
+    has_one = beneficiary,
+    has_one = vesting_account,
+  )]
+  pub employee_account : Account<'info, EmployeeAccount>,
+
+  #[account(
+    mut,
+    seeds = [company_name.as_ref()],
+    bump = vesting_account.bump,
+    has_one = treasury_token_account,
+    has_one = mint
+  )]
+  pub vesting_account : Account<'info, VestingAccount>,
+
+  pub mint : InterfaceAccount<'info, Mint>,
+
+  #[account(mut)]
+  pub treasury_token_account : InterfaceAccount<'info, TokenAccount>,
+
+  #[account(
+    init_if_needed,
+    payer = beneficiary,
+    associated_token::mint = mint,
+    associated_token::authority = beneficiary,
+    associated_token::token_program = token_program
+  )]
+  pub employee_token_account : InterfaceAccount<'info, TokenAccount>,
+
+  pub token_program : Interface<'info, TokenInterface>,
+  pub system_program : Program<'info, System>,
+  pub associated_token_program : Program<'info, AssociatedToken>
+}
 
